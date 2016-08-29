b1007952e47e3a6aedea8ee4690cd2f477e79a65
wangweizhouking <764676986@qq.com>
2016-08-29 14:42:35 +0800

    Initial commit

---
+++ app/src/main/java/com/yztc/wl/ap/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yztc.wl.ap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
